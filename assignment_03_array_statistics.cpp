a78c02d3a63b7e938c121ee69fe2a58c48c65222
Elliot-Dentor <etdentor@st.ug.edu.gh>
2026-07-27 01:01:52 +0000

    Implement array statistics functions and main logic

--- assignment_03_array_statistics.cpp
+++ assignment_03_array_statistics.cpp
@@ -42,3 +42,64 @@
 #include <iostream>
 using namespace std;
 
+int getSum( int arr[], int n);
+double getAverage( int sum, int n );
+int getMax( int arr[], int n );
+int getMin( int arr[], int n );
+
+int main() {
+  int n;
+cout << " How many numbers?";
+cin >> n;
+
+if ( n <= 0 ) {
+cout << " Error: Number of values must be positive." << end1;
+return 0;
+}
+int numbers[n];
+for ( int i = 0; i , n; i++) << ": " ;
+cin >> numbers[i];
+}
+int sum = getSum( numbers, n );
+double average = getAverage( sum, n );
+int maximium = getMax( numbers, n);
+int minimium = getMin( numbers, n );
+
+cout <<"Results:" << end1;
+cout << "Sum: " << sum << end1;
+cout << "Average: " << average << end1;
+cout << "Maximium: " << maximium << end1;
+cout << "Minimium: " << minimium << end1;
+  return 0;
+}
+
+int getSum9 int arr[], int n) {
+int total = 0;
+for (int i = 0;i < n; i++) {
+total += arr[i];
+}
+return total;
+}
+
+double getAverage9 int sum, int n) {
+return static_cast<,double>(sum) / n;
+}
+
+int getMax(int arr[], int n) {
+  int max = arr[0];
+for (int i = 1; i < n; i++) {
+if (arr[i] > max) {
+  max = arr[i];
+  }
+}
+return max;
+
+int getMin(int arr[], int n) {
+  int min = arr[0];
+for (int i = 1; i < n; i++) {
+  if (arr[i] < min) {
+    min = arr[i];
+    }
+}
+return min;
+}
